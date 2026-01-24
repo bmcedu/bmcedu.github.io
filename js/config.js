17f9338286251a3a38ef1fa1610f1285195ec4fd
Barham Barham <hello@barham.me>
2026-01-24 05:19:47 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Configuration
 const CONFIG = {
     // URL for Backend (Modular Single Script)
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyil9nKu1jk_V-eSkivc4cZquKVhmRzZk4J1RMAXHh9Sdu5DcwRos5OCrhnv9nlgO_9kw/exec'
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxosPZxu80g8aS3aRNp_ncTUkDSDTJXN9vjnp1_TOAEemRFkJWnYi69HUpgCzIaiDHrPw/exec'
 };
