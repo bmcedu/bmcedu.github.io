253b0964b81722c86962ee7ee7c23823e96908f6
Barham Barham <hello@barham.me>
2026-02-07 07:56:52 +0300

    fix(deployment): use new fresh backend deployment to resolve CORS issue

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration
 const CONFIG = {
     // URL for Backend (Modular Single Script)
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxZsI_DJcoq4XNml27_tOhQhYhBN7osPKjVqiITfc5RjUDoCRByf8iex1lKL7XMg4Dg3w/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxskkizmks7OeeWtIUR7BXW05acX3XOucTgWku9kV6DXM88y0AeRyxVYvuyielRx8YMuw/exec',
 
     // Branding
     // Colors are now handled via CSS (custom.css)
