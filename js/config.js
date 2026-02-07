a919b5781afdfe5d80039f4fedbca70251045c42
Barham Barham <hello@barham.me>
2026-02-07 05:15:52 +0300

    chore: Update backend script URL in config.js.

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration
 const CONFIG = {
     // URL for Backend (Modular Single Script)
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxCaHJy3uN3YmWdkrsFRXsPL27zw1ARF5N7vbMk-V_OeHlBKlge3LX77qVYTZGMP73N1w/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyU4IWtrJgWlGFNXG4nHv9LurF1CTHE1203bZqtsE0kfcTUlBRwPTuuJCTArpK24QdYaQ/exec',
 
     // Branding
     // Colors are now handled via CSS (custom.css)
