90b38b7ab2a3f427ff1a1c5c1ced9b6dd671dfbc
Barham Barham <hello@barham.me>
2026-02-07 04:57:53 +0300

    update

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration
 const CONFIG = {
     // URL for Backend (Modular Single Script)
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby8ZzmxTWaPonthtXG1kIwFlC_doabH5lMGdBSm6pDuzuSQgPKAY9GrDWEpzjX5v6njig/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby-sNGTdX5ZpvkiJc8xYfWd6K-q6zcFHA8dZ1AyQIB7iXoyAdibhefYhR9aUWMMIxQXjA/exec',
 
     // Branding
     // Colors are now handled via CSS (custom.css)
